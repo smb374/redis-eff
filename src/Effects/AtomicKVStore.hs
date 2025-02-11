ca4b6b911e4e46ed6b33e3c1a311497d4be76fa6
Po-Yeh Chen <poyehchen@cs.nycu.edu.tw>
2025-02-10 20:55:40 -0500

    Update lts version.

--- src/Effects/AtomicKVStore.hs
+++ src/Effects/AtomicKVStore.hs
@@ -37,40 +37,40 @@ type instance DispatchOf (AtomicKVStore k v) = Dynamic
 
 makeEffect ''AtomicKVStore
 
-writeKV :: AtomicKVStore k v :> es => k -> v -> Eff es ()
+writeKV :: (AtomicKVStore k v :> es) => k -> v -> Eff es ()
 writeKV k = updateKV k . Just
 {-# INLINE writeKV #-}
 
-deleteKV :: AtomicKVStore k v :> es => k -> Eff es ()
+deleteKV :: (AtomicKVStore k v :> es) => k -> Eff es ()
 deleteKV k = updateKV k Nothing
 {-# INLINE deleteKV #-}
 
-lookupOrThrowKV :: (AtomicKVStore k v :> es, Error e :> es) => (k -> e) -> k -> Eff es v
+lookupOrThrowKV :: (AtomicKVStore k v :> es, Error e :> es, Show e) => (k -> e) -> k -> Eff es v
 lookupOrThrowKV f k = do
     res <- lookupKV k
     case maybe (Left $ f k) Right res of
         Left e -> throwError e
         Right v -> return v
 
-existsKV :: AtomicKVStore k v :> es => k -> Eff es Bool
+existsKV :: (AtomicKVStore k v :> es) => k -> Eff es Bool
 existsKV = fmap isJust . lookupKV
 
-modifyKV
-    :: AtomicKVStore k v :> es
-    => v
-    -- ^ Default value if the key isn't present
-    -> (v -> v)
-    -> k
-    -> Eff es ()
+modifyKV ::
+    (AtomicKVStore k v :> es) =>
+    -- | Default value if the key isn't present
+    v ->
+    (v -> v) ->
+    k ->
+    Eff es ()
 modifyKV d f k =
     lookupKV k >>= \case
         Just v -> writeKV k $ f v
         Nothing -> writeKV k $ f d
 
-runAtomicKVStoreWithHashMap
-    :: (Hashable k, RS.Reader (TVar (HashMap k v)) :> es, Concurrent :> es)
-    => Eff (AtomicKVStore k v : es) a
-    -> Eff es a
+runAtomicKVStoreWithHashMap ::
+    (Hashable k, RS.Reader (TVar (HashMap k v)) :> es, Concurrent :> es) =>
+    Eff (AtomicKVStore k v : es) a ->
+    Eff es a
 runAtomicKVStoreWithHashMap = interpret $ \_ -> \case
     LookupKV k -> do
         store <- RS.ask
